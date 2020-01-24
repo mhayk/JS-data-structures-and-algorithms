dd937f0f061ff4858d2a1e0349c04d6a76013f57
Mhayk Whandson <hi@mhayk.com>
2020-01-24 22:55:30 +0000

    Stask with object based has been finished.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- stack-object-based/index.js
+++ stack-object-based/index.js
@@ -35,13 +35,32 @@ class Stack {
     }
     return this.items[this.count - 1]
   }
+
+  clear() {
+    this.items = {}
+    this.count = 0
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return ''
+    }
+
+    let objString = `${this.items[0]}`
+    for (let i = 1; i < this.count; i++) {
+      objString = `${objString},${this.items[i]}`
+    }
+    return objString
+  }
 }
 
 const myDishes = new Stack
 myDishes.push(2)
+myDishes.push(3)
 myDishes.push(3)
 myDishes.push(6)
 console.log(myDishes.pop())
 console.log(myDishes.items)
 console.log(myDishes.count)
 console.log(myDishes.peek())
+console.log(myDishes.toString())
